d33b0c7ce2071ca9526a38277bf5476861a04048
Marcos Mantovani <mantovani.marcos@gmail.com>
2024-06-19 23:09:38 -0300

    persist watch list in local storage. Initialize state using a callback function. Add css to expand/contract watch list in the mobile version. Change `vh` to `dvh` this only accounts for visible area (discard browser's search bar)

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import { useEffect, useState } from 'react'
 import StarRating from './StarRating'
 
-const tempMovieData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg'
-  },
-  {
-    imdbID: 'tt0133093',
-    Title: 'The Matrix',
-    Year: '1999',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'
-  },
-  {
-    imdbID: 'tt6751668',
-    Title: 'Parasite',
-    Year: '2019',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg'
-  }
-]
-
-const tempWatchedData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10
-  },
-  {
-    imdbID: 'tt0088763',
-    Title: 'Back to the Future',
-    Year: '1985',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9
-  }
-]
-
 const average = arr => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
 const KEY = 'bb268ed2'
 
 export default function App() {
   const [movies, setMovies] = useState([])
-  const [watched, setWatched] = useState([])
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem('watched')
+    return JSON.parse(storedValue) ?? []
+  })
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('')
   const [query, setQuery] = useState('')
@@ -80,6 +41,10 @@ export default function App() {
     setError('')
     setIsLoading(false)
   }
+
+  useEffect(() => {
+    localStorage.setItem('watched', JSON.stringify(watched))
+  }, [watched])
 
   async function fetchMovies(abort = null) {
     try {
@@ -215,11 +180,25 @@ function Main({ children }) {
 function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true)
 
+  function handleOpen() {
+    setIsOpen(open => !open)
+  }
+
+  useEffect(() => {
+    document.querySelector('main').classList.toggle('close-list')
+  }, [isOpen])
+
   return (
     <div className="box">
-      <button className="btn-toggle" onClick={() => setIsOpen(open => !open)}>
+      <button className="btn-toggle" onClick={handleOpen}>
         {isOpen ? '–' : '+'}
       </button>
+      <p className="open-watch-list-label info" onClick={handleOpen}>
+        <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
+          <path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
+        </svg>
+        Open watch list
+      </p>
       {isOpen && children}
     </div>
   )
@@ -260,8 +239,8 @@ function Movie({ movie, selectedId, onSelectMovie }) {
 function MovieDetails({ selectedId, onCloseMovie, watched, onAddWatched }) {
   const [movie, setMovie] = useState({})
   const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState('')
   const [userRating, setUserRating] = useState('')
+  const [error, setError] = useState('')
 
   const isWatched = watched.map(movie => movie.imdbID).includes(selectedId)
 
